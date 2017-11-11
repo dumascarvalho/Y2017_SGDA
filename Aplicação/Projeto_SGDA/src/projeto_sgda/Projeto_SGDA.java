595a168287707d57f3832d31b5a489218331c6bc
dumascarvalho <dumasmcarvalho@gmail.com>
2017-11-11 17:05:34 -0200

    Arquivos iniciais do projeto

---
+++ Aplicação/Projeto_SGDA/src/projeto_sgda/Projeto_SGDA.java
@@ -0,0 +1,17 @@
+package projeto_sgda;
+/*
+    DESENVOLVEDORES: Dumas Morais de Carvalho e Maria Eduarda de Oliveira Silva
+    DISCIPLINA: Linguagem de Programação II
+    PROFESSOR: Leandro Camara Ledel
+    CURSO: Análise e Desenvolvimento de Sistemas - 2º Semestre (Noturno)
+    INSTITUIÇÃO: Instituto Federal de São Paulo, Câmpus Hortolândia
+*/
+public class Projeto_SGDA {
+
+    public static void main(String[] args) {
+
+        
+        
+    }
+    
+}
